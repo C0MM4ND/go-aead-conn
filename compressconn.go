1950f8a9fcc519dc23c322efdf7f1438e7d928f3
Command <maoxs2@gmail.com>
2020-05-30 23:06:51 +0800

    use pipe and ioCopy to transfer data

--- compressconn.go
+++ compressconn.go
@@ -2,6 +2,7 @@ package aeadconn
 
 import (
 	"crypto/cipher"
+	"io"
 	"net"
 
 	"github.com/klauspost/compress/s2"
@@ -11,28 +12,46 @@ import (
 // AEADCompressConn uses https://github.com/klauspost/compress/tree/master/s2#s2-compression to compress the data
 type AEADCompressConn struct {
 	net.Conn
-	*stream.StreamWriteCloser
-	*stream.StreamReader
+
+	compressW io.Writer
+	compressR io.Reader
+
+	pipeR *io.PipeReader
+	pipeW *io.PipeWriter
+
+	cryptoW io.Writer
+	cryptoR io.Reader
 }
 
+// raw <--> 1. Encode <--> 2. Encrypt(Write) <-ciphertext-> 1. Decrypt(Read) <--> 2. Decode <--> raw
 func NewAEADCompressConn(seed []byte, chunkSize int, conn net.Conn, aead cipher.AEAD) *AEADCompressConn {
+	pipeR, pipeW := io.Pipe()
+
 	return &AEADCompressConn{
-		Conn:              conn,
-		StreamWriteCloser: stream.NewStreamWriteCloser(seed, chunkSize, conn, aead),
-		StreamReader:      stream.NewStreamReader(seed, chunkSize, conn, aead),
+		Conn: conn,
+
+		compressW: s2.NewWriter(pipeW),
+		compressR: s2.NewReader(pipeR),
+
+		pipeR: pipeR,
+		pipeW: pipeW,
+
+		cryptoW: stream.NewStreamWriteCloser(seed, chunkSize, conn, aead),
+		cryptoR: stream.NewStreamReader(seed, chunkSize, conn, aead),
 	}
 }
 
 func (cc *AEADCompressConn) Close() error {
-	return cc.StreamWriteCloser.Close()
+	return cc.Conn.Close()
 }
 
-func (cc *AEADCompressConn) Write(b []byte) (int, error) {
-	b = s2.EncodeBetter(nil, b)
-	return cc.StreamWriteCloser.Write(b)
+func (cc *AEADCompressConn) Write(b []byte) (i int, err error) {
+	i, err = cc.compressW.Write(b)
+	io.Copy(cc.cryptoW, cc.pipeR)
+	return
 }
 
 func (cc *AEADCompressConn) Read(b []byte) (int, error) {
-	b, _ = s2.Decode(nil, b)
-	return cc.StreamReader.Read(b)
+	io.Copy(cc.pipeW, cc.cryptoR)
+	return cc.compressR.Read(b)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/maoxs2/go-aead-conn
 go 1.14
 
 require (
-	github.com/andybalholm/brotli v1.0.0
+	github.com/klauspost/compress v1.10.6
 	github.com/maoxs2/go-aead-iostream v0.0.0-20200530085108-b119dc15cecf
+	github.com/mmcloughlin/avo v0.0.0-20200523190732-4439b6b2c061 // indirect
 )
